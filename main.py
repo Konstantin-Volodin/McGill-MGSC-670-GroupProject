f7b077efd40978ae2c2ac4b8347199ddddcfcf27
Konstantin-Volodin <63243086+Konstantin-Volodin@users.noreply.github.com>
2023-05-11 16:52:56 -0400

    added visualizations

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ df_dist = df_dist.\
          'sales_sd': ['mean', 'std']}).\
     reset_index()
 df_dist.columns = ['price', 'mean_mean', 'mean_sd', 'sd_mean', 'sd_sd']
-df_dist
+DF_DIST = df_dist.set_index('price').to_dict('index')
 
 # %% DISTRIBUTIONS of TRANSITIONS
 df_trn = df.groupby(['item', 'price']).\
@@ -39,7 +39,8 @@ df_trn = df_trn.groupby('price').\
     reset_index()
 df_trn.columns = ['price', 'mean_change_mean', 'mean_change_sd',
                   'sd_change_mean', 'sd_change_sd']
-df_trn
+DF_TRN = df_trn.set_index('price').to_dict('index')
+DF_TRN
 
 # %%
 
@@ -69,24 +70,23 @@ def simulator(distr, policy):
         hist_weeks.append(i+1)
 
         # Action
-        old_price = curr_price
         curr_price = policy(curr_price, hist_sales, hist_prices)
 
-        # Report
-        # print(f"Week: {i+1}")
-        # print(f"\tPrice: {old_price:.2f}")
-        # print(f"\tSales: {sales:.2f}")
-        # print(f"\tRemaining Inv: {curr_inventory:.2f}")
-
     return (hist_weeks, hist_sales, hist_prices)
-    # print(f"\tPrice New: {curr_price}")
 
 
 def baseline_policy(curr_price, sales, prices):
     return (curr_price)
 
 
-def likelihood_estimator(curr_price, sales, prices):
+def decreate_policy(curr_price, sales, prices):
+    if curr_price == 36:
+        return (ACTIONS[curr_price][0])
+    else:
+        return (ACTIONS[curr_price][1])
+
+
+def naive_likelihood_policy(curr_price, sales, prices):
     # Get Price Relevant Data
     indexes = np.where(np.array(prices) == curr_price)[0]
 
@@ -112,11 +112,30 @@ def likelihood_estimator(curr_price, sales, prices):
         return (ACTIONS[curr_price][0])
 
 
-def decreate_policy(curr_price, sales, prices):
-    if curr_price == 36:
-        return (ACTIONS[curr_price][0])
-    else:
+def mean_likelihood_policy(curr_price, sales, prices):
+    # Get Price Relevant Data
+    indexes = np.where(np.array(prices) == curr_price)[0]
+
+    # Need to gather more data
+    if len(indexes) <= 1:
+        return (curr_price)
+
+    # Get Distribution Estimate
+    curr_mean = np.mean(np.array(sales)[indexes])
+    curr_sd = np.std(np.array(sales)[indexes])
+
+    # Get Sales Requirements
+    left_over_inv = TOTAL_INV - sum(sales)
+    daily_req = left_over_inv/(TOTAL_DUR-len(sales))
+
+    # Check if probability meets it
+    prob_it_fits = 1-scs.norm.cdf(daily_req, loc=curr_mean, scale=curr_sd)
+
+    if prob_it_fits < LIKELIHOOD_ESTIMATOR_PROB and curr_price != 36:
         return (ACTIONS[curr_price][1])
+
+    else:
+        return (ACTIONS[curr_price][0])
 
 
 # %% FOR TESTING
@@ -128,7 +147,7 @@ TOTAL_INV = 2000
 TOTAL_DUR = 15
 LIKELIHOOD_ESTIMATOR_PROB = 0.1
 
-distributions = {60: {'mean': max(np.random.normal(73.897354, 19.646415), 0),
+distributions = {60: {'mean': max(np.random.normal(DF_TRN[36]['mean_change_mean'], 19.646415), 0),
                       'sd': max(np.random.normal(21.363060, 10.900014), 0)},
                  54: {'mean': max(np.random.normal(98.777778, 29.547931), 0),
                       'sd': max(np.random.normal(34.059664, 15.195045), 0)},
@@ -137,7 +156,7 @@ distributions = {60: {'mean': max(np.random.normal(73.897354, 19.646415), 0),
                  36: {'mean': max(np.random.normal(181.366667, 12.537942), 0),
                       'sd': max(np.random.normal(79.846521, 46.166815), 0)}, }
 
-ind_res = simulator(distributions, likelihood_estimator)
+ind_res = simulator(distributions, naive_likelihood_policy)
 
 # %% 100 simulations
 ACTIONS = {60: [60, 54, 48, 36],
@@ -145,11 +164,11 @@ ACTIONS = {60: [60, 54, 48, 36],
            48: [48, 36],
            36: [36]}
 TOTAL_INV = 2000
-TOTAL_DUR = 20
+TOTAL_DUR = 15
 LIKELIHOOD_ESTIMATOR_PROB_OPTIONS = np.linspace(0, 1, 101)
 
 res = {'repl': [], 'week': [], 'sales': [], 'price': [], 'policy': [], 'param': []}
-for i in tqdm(range(50)):
+for i in tqdm(range(500)):
     distributions = {60: {'mean': max(np.random.normal(73.897354, 19.646415), 0),
                           'sd': max(np.random.normal(21.363060, 10.900014), 0)},
                      54: {'mean': max(np.random.normal(98.777778, 29.547931), 0),
@@ -179,7 +198,7 @@ for i in tqdm(range(50)):
 
     # LIKELIHOOD
     for LIKELIHOOD_ESTIMATOR_PROB in LIKELIHOOD_ESTIMATOR_PROB_OPTIONS:
-        ind_res = simulator(distributions, likelihood_estimator)
+        ind_res = simulator(distributions, naive_likelihood_policy)
         res['repl'].extend([i for j in range(TOTAL_DUR)])
         res['policy'].extend(['likelihood' for j in range(TOTAL_DUR)])
         res['param'].extend([LIKELIHOOD_ESTIMATOR_PROB for j in range(TOTAL_DUR)])
@@ -189,10 +208,39 @@ for i in tqdm(range(50)):
 
 res = pd.DataFrame(res)
 res['revenue'] = res['sales'] * res['price']
-# %%
+
+# %% Aggregated results
 res_agg = res.groupby(['policy', 'param', 'repl']).agg({'revenue': 'sum'}).reset_index()
 res_agg = res_agg.groupby(['policy', 'param']).agg({'revenue': ['mean', 'std']}).reset_index()
 res_agg.columns = ['policy', 'param', 'revenue_mean', 'revenue_sd']
 res_agg.sort_values('revenue_mean')
-# res
+
+# %% Plot evolutions
+res_baseline = res.query(f'policy == "baseline"')
+res_baseline['cum_revenue'] = res_baseline.groupby('repl')['revenue'].transform(pd.Series.cumsum)
+
+res_decrease = res.query(f'policy == "always_decrease"')
+res_decrease['cum_revenue'] = res_decrease.groupby('repl')['revenue'].transform(pd.Series.cumsum)
+
+res_likelihood = res.query(f'policy == "likelihood" and param == 0.03')
+res_likelihood['cum_revenue'] = res_likelihood.groupby('repl')['revenue'].transform(pd.Series.cumsum)
+
+res_all = pd.concat([res_baseline, res_decrease, res_likelihood])
+
+# Simulations
+fig = px.line(res_all, x='week', y='cum_revenue', color='policy',
+                facet_col='policy', facet_col_wrap=2,
+              )
+fig.update_traces(opacity=0.05)
+fig.show(renderer='browser')
+
+# Aggregate
+res_all_agg = res_all.\
+    groupby(['week', 'policy']).\
+    agg({'cum_revenue': 'mean'}).\
+    reset_index()
+
+fig = px.line(res_all_agg, x='week', y='cum_revenue', color='policy',)
+fig.show(renderer='browser')
+
 # %%
